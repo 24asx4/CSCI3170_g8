f013d92f4aa4b407db00bc215ca46b19c4f136af
24asx4 <24asasasas@gmail.com>
2023-11-19 02:45:12 +0800

    Only menu and connection is done

---
+++ salesSystem/Main.java
@@ -0,0 +1,57 @@
+package salesSystem;
+import java.io.*;
+import java.sql.*;
+import java.sql.Connection;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db8?autoReconnect=true&useSSL=false";
+    public static String dbUsername = "Group8";
+    public static String dbPassword = "CSCI3170";
+
+    public static Connection connectToMySQL(){
+            Connection con = null;
+            try{
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+            } catch (ClassNotFoundException e){
+                    System.out.println("[Error]: Java MySQL DB Driver not found!!");
+                    System.exit(0);
+            } catch (SQLException e){
+                    System.out.println(e);
+            }
+            return con;
+    }
+
+
+    public static void main(String[] args){
+        connectToMySQL();
+        int choice=0;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome to sales system!");
+        while (choice!=4) {
+            Menu menu=new Menu();
+            menu.mainMenu();
+            choice=input.nextInt();
+            switch (choice) {
+                case 1:
+                    menu.adminMenu();
+                    break;
+                case 2:
+                    menu.salesPersonMenu();
+                    break;
+                case 3:
+                    menu.mangerMenu();
+                    break;
+                case 4:
+                    ;
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        }
+    }    
+}
---
+++ salesSystem/Menu.java
@@ -0,0 +1,114 @@
+package salesSystem;
+import java.io.*;
+import java.util.Scanner;
+
+public class Menu{
+    private int choice;
+    public Menu(){
+        choice=0;
+    }
+    public void mainMenu(){
+        System.out.println("-----Main Menu-----");
+        System.out.println("What kinds of operation would you like to perform?");
+        System.out.println("1. Operations for administrator");
+        System.out.println("2. Operations for salesperson");
+        System.out.println("3. Operations for manger");
+        System.out.println("4. Exit this program");
+        System.out.print("Enter Your Choice: ");
+    }
+    public void salesPersonMenu(){
+        while (this.choice==0){
+            System.out.println("-----Operations for salesperson menu-----");
+            System.out.println("What kinds of operation would you like to perform?");
+            System.out.println("1. Search for parts");
+            System.out.println("2. Sell a part");
+            System.out.println("3. Return to main menu");
+            System.out.print("Enter Your Choice: ");
+            Scanner input=new Scanner(System.in);
+            this.choice=input.nextInt();
+            switch (this.choice) {
+                case 1:
+                    ;
+                    break;
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+                    this.choice=0;
+                    break;
+            }
+        }
+    }
+    public void mangerMenu(){
+        while (this.choice==0){
+            System.out.println("-----Operations for manger menu-----");
+            System.out.println("What kinds of operation would you like to perform?");
+            System.out.println("1. List all salespersons");
+            System.out.println("2. Count the no. of record of each salesperson under a specific range on years of experience");
+            System.out.println("3. Show the total sale value of each manufacturer");
+            System.out.println("4. Show the N most popular part");
+            System.out.println("5. Return to main menu");
+            System.out.print("Enter Your Choice: ");
+            Scanner input=new Scanner(System.in);
+            this.choice=input.nextInt();
+            switch (this.choice) {
+                case 1:
+                    ;
+                    break;
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    ;
+                    break;
+                case 4:
+                    ;
+                    break;
+                case 5:
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+                    this.choice=0;
+                    break;
+            }
+        }
+    }    
+    public void adminMenu(){
+        while (this.choice==0){
+            System.out.println("-----Operations for administrator menu-----");
+            System.out.println("What kinds of operation would you like to perform?");
+            System.out.println("1. Create all tables");
+            System.out.println("2. Delete all tables");
+            System.out.println("3. Load from datafile");
+            System.out.println("4. Show content of a table");
+            System.out.println("5. Return to main menu");
+            System.out.print("Enter Your Choice: ");
+            Scanner input=new Scanner(System.in);
+            this.choice=input.nextInt();
+            switch (this.choice) {
+                case 1:
+                    ;
+                    break;
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    ;
+                    break;
+                case 4:
+                    ;
+                    break;
+                case 5:
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+                    this.choice=0;
+                    break;
+            }
+        }
+    }
+        
+}
