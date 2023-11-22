4332a55a5031342adfcea480c0ee6c12e1bc4b6c
Ally Chan <1155159600@link.cuhk.edu.hk>
2023-11-22 10:19:38 +0800

    Admin operations 1-2

--- salesSystem/Main.java
+++ salesSystem/Main.java
@@ -1,40 +1,38 @@
 package salesSystem;
+
 import java.io.*;
 import java.sql.*;
 import java.sql.Connection;
 import java.util.Scanner;
-
-
 
 public class Main {
     public static String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db8?autoReconnect=true&useSSL=false";
     public static String dbUsername = "Group8";
     public static String dbPassword = "CSCI3170";
 
-    public static Connection connectToMySQL(){
-            Connection con = null;
-            try{
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
-            } catch (ClassNotFoundException e){
-                    System.out.println("[Error]: Java MySQL DB Driver not found!!");
-                    System.exit(0);
-            } catch (SQLException e){
-                    System.out.println(e);
-            }
-            return con;
+    public static Connection connectToMySQL() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+        } catch (ClassNotFoundException e) {
+            System.out.println("[Error]: Java MySQL DB Driver not found!!");
+            System.exit(0);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return con;
     }
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Connection con = connectToMySQL();
-        int choice=0;
-        Scanner input=new Scanner(System.in);
+        int choice = 0;
+        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to sales system!");
-        while (choice!=4) {
-            Menu menu=new Menu();
+        while (choice != 4) {
+            Menu menu = new Menu();
             menu.mainMenu();
-            choice=input.nextInt();
+            choice = input.nextInt();
             switch (choice) {
                 case 1:
                     menu.adminMenu(con);
@@ -53,5 +51,5 @@ public class Main {
                     break;
             }
         }
-    }    
+    }
 }
--- salesSystem/Menu.java
+++ salesSystem/Menu.java
@@ -1,14 +1,17 @@
 package salesSystem;
+
 import java.io.*;
 import java.sql.*;
 import java.util.Scanner;
 
-public class Menu{
+public class Menu {
     private int choice;
-    public Menu(){
-        choice=0;
+
+    public Menu() {
+        choice = 0;
     }
-    public void mainMenu(){
+
+    public void mainMenu() {
         System.out.println();
         System.out.println("-----Main Menu-----");
         System.out.println("What kinds of operation would you like to perform?");
@@ -18,8 +21,9 @@ public class Menu{
         System.out.println("4. Exit this program");
         System.out.print("Enter Your Choice: ");
     }
-    public void salesPersonMenu(Connection con){
-        while (this.choice==0){
+
+    public void salesPersonMenu(Connection con) {
+        while (this.choice == 0) {
             System.out.println();
             System.out.println("-----Operations for salesperson menu-----");
             System.out.println("What kinds of operation would you like to perform?");
@@ -27,8 +31,8 @@ public class Menu{
             System.out.println("2. Sell a part");
             System.out.println("3. Return to main menu");
             System.out.print("Enter Your Choice: ");
-            Scanner input=new Scanner(System.in);
-            this.choice=input.nextInt();
+            Scanner input = new Scanner(System.in);
+            this.choice = input.nextInt();
             switch (this.choice) {
                 case 1:
                     ;
@@ -40,25 +44,27 @@ public class Menu{
                     return;
                 default:
                     System.out.println("Invalid choice!");
-                    this.choice=0;
+                    this.choice = 0;
                     break;
             }
         }
     }
-    public void managerMenu(Connection con){
-        while (this.choice==0){
+
+    public void managerMenu(Connection con) {
+        while (this.choice == 0) {
             System.out.println();
             System.out.println("-----Operations for manager menu-----");
             System.out.println("What kinds of operation would you like to perform?");
             System.out.println("1. List all salespersons");
-            System.out.println("2. Count the no. of record of each salesperson under a specific range on years of experience");
+            System.out.println(
+                    "2. Count the no. of record of each salesperson under a specific range on years of experience");
             System.out.println("3. Show the total sale value of each manufacturer");
             System.out.println("4. Show the N most popular part");
             System.out.println("5. Return to main menu");
             System.out.print("Enter Your Choice: ");
-            Scanner input=new Scanner(System.in);
-            this.choice=input.nextInt();
-            Manager manager=new Manager();
+            Scanner input = new Scanner(System.in);
+            this.choice = input.nextInt();
+            Manager manager = new Manager();
             switch (this.choice) {
                 case 1:
                     manager.listSalesperson(con);
@@ -76,13 +82,14 @@ public class Menu{
                     return;
                 default:
                     System.out.println("Invalid choice!");
-                    this.choice=0;
+                    this.choice = 0;
                     break;
             }
         }
-    }    
-    public void adminMenu(Connection con){
-        while (this.choice==0){
+    }
+
+    public void adminMenu(Connection con) {
+        while (this.choice == 0) {
             System.out.println();
             System.out.println("-----Operations for administrator menu-----");
             System.out.println("What kinds of operation would you like to perform?");
@@ -92,14 +99,39 @@ public class Menu{
             System.out.println("4. Show content of a table");
             System.out.println("5. Return to main menu");
             System.out.print("Enter Your Choice: ");
-            Scanner input=new Scanner(System.in);
-            this.choice=input.nextInt();
+            Scanner input = new Scanner(System.in);
+            this.choice = input.nextInt();
             switch (this.choice) {
                 case 1:
-                    ;
+                    try {
+                        Statement stmt = con.createStatement();
+                        String sql = "CREATE TABLE IF NOT EXISTS category (cID int NOT NULL, cName varchar(20) NOT NULL, PRIMARY KEY(cID), CHECK (cID>=1 AND cID<=9))";
+                        stmt.executeUpdate(sql);
+                        sql = "CREATE TABLE IF NOT EXISTS manufacturer (mID int NOT NULL, mName varchar(20) NOT NULL, mAddress varchar(50) NOT NULL, mPhoneNumber int NOT NULL, PRIMARY KEY(mID), CHECK (mID<=99 AND mPhoneNumber>=10000000 AND mPhoneNumber<=99999999))";
+                        stmt.executeUpdate(sql);
+                        sql = "CREATE TABLE IF NOT EXISTS part (pID int NOT NULL, pName varchar(20) NOT NULL, pPrice int NOT NULL, mID int NOT NULL, cID int NOT NULL, pWarranty int NOT NULL, pAvailableQuantity int NOT NULL, PRIMARY KEY(pID), FOREIGN KEY(mID) REFERENCES manufacturer(mID), FOREIGN KEY(cID) REFERENCES category(cID), CHECK (pID<=999 AND mPrice<=99999 AND pWarranty<=99 AND pAvailableQuantity<=99))";
+                        stmt.executeUpdate(sql);
+                        sql = "CREATE TABLE IF NOT EXISTS salesperson (sID int NOT NULL, sName varchar(20) NOT NULL, sAddress varchar(50) NOT NULL, sPhoneNumber int NOT NULL, sExperience int NOT NULL, PRIMARY KEY(sID), CHECK (sID<=99 AND sPhoneNumber>=10000000 AND sPhoneNumber<=99999999 AND sExperience>=1 AND sExperience<=9))";
+                        stmt.executeUpdate(sql);
+                        sql = "CREATE TABLE IF NOT EXISTS transaction (tID int NOT NULL, pID int NOT NULL, sID int NOT NULL, tDate varchar(10) NOT NULL, PRIMARY KEY(tID), FOREIGN KEY(pID) REFERENCES part(pID), FOREIGN KEY(sID) REFERENCES salesperson(sID), CHECK (tID<=9999))";
+                        stmt.executeUpdate(sql);
+                        System.out.println("Processing...Done! Database is initialized!");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case 2:
-                    ;
+                    try {
+                        Statement stmt = con.createStatement();
+                        String[] tables = { "transaction", "salesperson", "part", "manufacturer", "category" };
+                        for (int i = 0; i < tables.length; i++) {
+                            String sql = "DROP TABLE IF EXISTS " + tables[i];
+                            stmt.executeUpdate(sql);
+                        }
+                        System.out.println("Processing...Done! Database is removed!");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case 3:
                     ;
@@ -111,9 +143,9 @@ public class Menu{
                     return;
                 default:
                     System.out.println("Invalid choice!");
-                    this.choice=0;
+                    this.choice = 0;
                     break;
             }
         }
-    }       
+    }
 }
